abaeaebf0855383377e86449f459fb32d7e63dab
humphreyokoth <humpasp@gmail.com>
2024-08-08 16:41:07 +0300

    feat:featured jobs

--- database/seeders/JobSeeder.php
+++ database/seeders/JobSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Job;
 use App\Models\Tag;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 
 class JobSeeder extends Seeder
 {
@@ -15,6 +16,12 @@ class JobSeeder extends Seeder
     public function run(): void
     {
        $tags = Tag::factory(3)->create();
-        Job::factory(20)->hasAttached($tags)->create();
+        Job::factory(20)->hasAttached($tags)->create(new Sequence([
+            'featured'=>false,
+            'schedule'=> 'Full Time',
+        ],[
+            'featured'=>True,
+            'schedule'=> 'Part Time',
+        ]));
     }
 }
