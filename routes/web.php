8bc1baff2f4101effd3a714de5d5b3a33349d82b
humphreyokoth <humpasp@gmail.com>
2024-08-09 13:52:04 +0300

    feat:auth

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\TagController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisteredUserController;
 
 
 
-Route::get('/', [JobController::class,'index']);
+
+
+Route::get('/', [JobController::class, 'index']);
 // Route::get('/tags/{tag:name}', [TagController::class,'']);
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisteredUserController::class, 'create']);
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+    Route::get('/login', [LoginController::class, 'create']);
+    Route::post('/login', [LoginController::class, 'store']);
+});
+
+
+Route::delete('/logout', [LoginController::class, 'destroy'])->middleware('auth');
